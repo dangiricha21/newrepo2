cc0785be66be33891132a6cc0342577b74d46ce2
dangiricha21 <dangi.richa21@gmail.com>
2026-04-26 01:15:06 +0530

    fix prisma datasource

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -16,9 +16,35 @@
 
 
 
+// import "dotenv/config";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+
+
+
+
+
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+console.log("PRISMA ENV:", process.env.DATABASE_URL);
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
